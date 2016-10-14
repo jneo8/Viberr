62e23f17aada25e45cda5a9a876f17807748d020
jneo8 <james@linzhixiandeMacBook-Air.local>
2016-10-14 21:24:46 +0800

    chapter3 basic testing return minimal HTML

--- superlist/tests.py
+++ superlist/tests.py
@@ -1,10 +1,22 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
+from django.http import HttpRequest
 from .views import home_page
 
 
 class HomePageTest(TestCase):
 
+    # 測試url /superlist/ 是否有連接到 homepage 這個function
     def test_root_url_resolvers(self):
         found = resolve('/superlist/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_return_correct_html(self):
+        # 建立HttpRequest物件
+        request = HttpRequest()
+        # 將request傳給home_page view, 會得到home_page回傳的response
+        response = home_page(request)
+        # response.content是原始位元組，必須使用 b'' 語法來進行比較
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
