bdef1623214913f8a2143eee9d18c9088d5e6d47
jneo8 <james@linzhixiandeMacBook-Air.local>
2016-10-15 00:24:54 +0800

    Refactoring views.py add home.html

--- superlist/tests.py
+++ superlist/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 from .views import home_page
 
 
@@ -16,7 +17,9 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         # 將request傳給home_page view, 會得到home_page回傳的response
         response = home_page(request)
+        expected_html = render_to_string('superlist/home.html')
+        self.assertEqual(response.content.decode(), expected_html)
         # response.content是原始位元組，必須使用 b'' 語法來進行比較
-        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertTrue(response.content.startswith(b'<!DOCTYPE html>'))
         self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        self.assertTrue(response.content.strip().endswith(b'</html>'))
--- superlist/views.py
+++ superlist/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 
 def home_page(request):
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    return render(request, 'superlist/home.html')
 
 # Create your views here.
