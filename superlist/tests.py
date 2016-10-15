e801fe48e7e508d9c29dd8946a9a412da07080c4
jneo8 <james@linzhixiandeMacBook-Air.local>
2016-10-16 07:14:48 +0800

    function_test.py get errors 提交後開始重構

--- superlist/tests.py
+++ superlist/tests.py
@@ -17,9 +17,28 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         # 將request傳給home_page view, 會得到home_page回傳的response
         response = home_page(request)
+        # 由於csrf token 所以會報錯
         expected_html = render_to_string('superlist/home.html')
         self.assertEqual(response.content.decode(), expected_html)
         # response.content是原始位元組，必須使用 b'' 語法來進行比較
         self.assertTrue(response.content.startswith(b'<!DOCTYPE html>'))
         self.assertIn(b'<title>To-Do lists</title>', response.content)
         self.assertTrue(response.content.strip().endswith(b'</html>'))
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertIn('A new list item', response.content.decode())
+
+        expected_html = render_to_string(
+            'superlist/home.html',
+            {'new_item_text': 'A new list item'}
+            )
+        # 由於csrf token 所以會報錯
+        self.assertEqual(response.content.decode(), expected_html)
+
+
--- superlist/views.py
+++ superlist/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def home_page(request):
-    return render(request, 'superlist/home.html')
+    return render(request, 'superlist/home.html', {
+        'new_item_text': request.POST.get('item_text', '')
+        })
 
 # Create your views here.
