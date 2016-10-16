9c7e7c1d6df42438a7ef215cae236dfc529572bd
jneo8 <james@linzhixiandeMacBook-Air.local>
2016-10-16 16:09:30 +0800

    /modify 	function_test.py 		/add 		check_for_row_in_list_table

--- function_test.py
+++ function_test.py
@@ -9,10 +9,16 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Safari()
-        self.browser.implicitly_wait(5)
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text+"\ue007", [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8090/superlist')
 
@@ -30,27 +36,29 @@ class NewVisitorTest(unittest.TestCase):
 
         # 在文字方塊輸入 'Buy peacock feathers'
         inputbox.send_keys('Buy peacock feathers')
-        # 按下enter時網頁會更新，並列出
-        # '1 : Buy peacock feathers'
+        # 按下enter時網頁會更新，並列出 '1 : Buy peacock feathers'
         inputbox.send_keys(Keys.ENTER)
+        # solution for StaleElementReferenceException
         time.sleep(2)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers\ue007', [row.text for row in rows])
-        
+        # 檢查項目是否存在table中
+        self.check_for_row_in_list_table('1: Buy peacock feathers')       
 
         # 此時仍然有另外一個文字方塊
-        # user 輸入 'another item'
         inputbox = self.browser.find_element_by_id('id_new_item')
+        # user 輸入 'Use peacock feathers to make a fly'
         inputbox.send_keys('Use peacock feathers to make a fly')
+        # POST and refresh page
         inputbox.send_keys(Keys.ENTER)
+        # solution for StaleElementReferenceException
         time.sleep(2)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        # self.assertIn('1: Buy peacock feathers\ue007', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly\ue007', [row.text for row in rows])
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')       
+
+
+        # table = self.browser.find_element_by_id('id_list_table')
+        # rows = table.find_elements_by_tag_name('tr')
+        # # self.assertIn('1: Buy peacock feathers\ue007', [row.text for row in rows])
+        # self.assertIn('2: Use peacock feathers to make a fly\ue007', [row.text for row in rows])
 
 
 
@@ -59,10 +67,6 @@ class NewVisitorTest(unittest.TestCase):
         # self.fail('finish the test!')
 
         # now user的清單有兩個項目
-
-
-
-
 
 
 
