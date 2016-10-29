dba5c58244b33dac6ae1114b40d4f3e6a7b3fa71
jneo8 <james@linzhixiandeMacBook-Air.local>
2016-10-29 21:39:24 +0800

    function_test/test.py 	+新的功能測試 	+使用REST規則 	+切換使用者

--- function_test/test.py
+++ function_test/test.py
@@ -22,6 +22,7 @@ class NewVisitorTest(LiveServerTestCase):
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
+        # 使用 find_elements_  可以找到複數個tr標籤，再用迴圈去搜尋
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
@@ -35,7 +36,7 @@ class NewVisitorTest(LiveServerTestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('Your To-Do lists', header_text)
 
-        # user 受邀輸入一個代辦事項
+        # user1 受邀輸入一個代辦事項
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
@@ -44,8 +45,13 @@ class NewVisitorTest(LiveServerTestCase):
 
         # 在文字方塊輸入 'Buy peacock feathers'
         inputbox.send_keys('Buy peacock feathers')
-        # 按下enter時網頁會更新，並列出 '1 : Buy peacock feathers'
+        # 按下enter時會帶使用者到新的URL
+        # 並且列出 1: Buy peacock feathers
+        # 一個待辦清單的項目
         inputbox.send_keys(Keys.ENTER)
+        user1_list_url = self.browser.current_url
+        # 檢查 user1_list_url 是否匹配正規表達式
+        self.assertRegex(user1_list_url, '/list/.+')
         # solution for StaleElementReferenceException
         time.sleep(2)
         # 檢查項目是否存在table中
@@ -53,17 +59,45 @@ class NewVisitorTest(LiveServerTestCase):
 
         # 此時仍然有另外一個文字方塊
         inputbox = self.browser.find_element_by_id('id_new_item')
-        # user 輸入 'Use peacock feathers to make a fly'
+        # user1 輸入 'Use peacock feathers to make a fly'
         inputbox.send_keys('Use peacock feathers to make a fly')
         # POST and refresh page
         inputbox.send_keys(Keys.ENTER)
         # solution for StaleElementReferenceException
         time.sleep(2)
 
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')       
+        # 再次更新網頁並檢查是否有兩個項目
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')       
+   
+        # 然後換成新的使用者user2進來;這邊先關掉遊覽器
+        self.browser.quit()
+        self.browser = webdriver.Chrome()
+
+        # user2造訪首頁並且清單內無顯示任何user1的Item
+        self.browser.get('%s%s' % (self.live_server_url, '/superlist'))
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+
+        # user2 輸入一個新的項目
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(2)
+
+        user2_list_url = self.browser.current_url
+        self.assertRegex(user2_list_url, '/list/.+')
+        # 確認user1的網址和user2的網址不相同
+        self.assertNotEqual(user1_list_url, user2_list_url)
+
+        #再次確認清單內沒有任何user1 的Item
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
 
 
-       
+
+
+     
         self.fail('finish the test!')
 
         # now user的清單有兩個項目
